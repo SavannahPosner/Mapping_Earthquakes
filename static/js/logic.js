aa664690b7077b32db69f24b1e4a4bc813220468
Savannah Posner <105872809+SavannahPosner@users.noreply.github.com>
2022-09-02 15:44:29 -0500

    updates

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,8 +1,30 @@
 // Create the map object with center at the San Francisco airport.
-let map = L.map('mapid').setView([37.6213, -122.3790], 5);
+// Create the map object with center at the San Francisco airport.
+let map = L.map('mapid').setView([37.5, -122.5], 10);
 
 
 
+ // Add GeoJSON data.
+ let sanFranAirport =
+ {"type":"FeatureCollection","features":[{
+     "type":"Feature",
+     "properties":{
+         "id":"3469",
+         "name":"San Francisco International Airport",
+         "city":"San Francisco",
+         "country":"United States",
+         "faa":"SFO",
+         "icao":"KSFO",
+         "alt":"13",
+         "tz-offset":"-8",
+         "dst":"A",
+         "tz":"America/Los_Angeles"},
+         "geometry":{
+             "type":"Point",
+             "coordinates":[-122.375,37.61899948120117]}}
+ ]};
+ 
+ L.geoJSON(sanFranAirport).addTo(map);
 
 // We create the tile layer that will be the background of our map.
 // We create the tile layer that will be the background of our map.
@@ -34,3 +56,9 @@ let line = [
 L.polyline(line, {
     color: "yellow"
   }).addTo(map)
+
+  L.geoJson(data, {
+    pointToLayer: function(feature, latlng) {
+      return L.marker(latlng);
+     }
+});
